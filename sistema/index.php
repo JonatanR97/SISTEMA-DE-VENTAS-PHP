1246d144cf2da9b4f98728c568734abcf5860514
Jonatan Regalado <jonatanregalado76@gmail.com>
2021-10-09 16:15:38 -0600

    Actualizacion al agregar producto

--- sistema/index.php
+++ sistema/index.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
 	<!-- Page Heading -->
 	<div class="d-sm-flex align-items-center justify-content-between mb-4">
-		<h1 class="h3 mb-0 text-gray-800">Panel de Administración</h1>
+		<h1 class="h3 mb-0 text-gray-800">PANEL DE ADMINISTRACION</h1>
 	</div>
 
 	<!-- Content Row -->
