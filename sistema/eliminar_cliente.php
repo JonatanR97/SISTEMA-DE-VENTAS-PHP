62fddbebbd89839374ffc21aac0607ce95bb73f3
Jonatan Regalado <jonatanregalado76@gmail.com>
2021-10-23 13:08:02 -0600

    Correccion de errores

---
+++ sistema/eliminar_cliente.php
@@ -0,0 +1,10 @@
+<?php
+if (!empty($_GET['id'])) {
+    require("../conexion.php");
+    $id = $_GET['id'];
+    $query_delete = mysqli_query($conexion, "DELETE FROM cliente WHERE idcliente = $id");
+    mysqli_close($conexion);
+    header("location: lista_cliente.php");
+    
+}
+?>
