e1f84d16f2d649e8aff9c3bd7641b1cab7483f90
Jonatan Regalado <jonatanregalado76@gmail.com>
2021-10-23 15:38:08 -0600

    Pruebas del sistema

--- sistema/lista_cliente.php
+++ sistema/lista_cliente.php
@@ -17,7 +17,7 @@
 					<thead class="thead-dark">
 						<tr>
 							<th>ID</th>
-							<th>DNI</th>
+							<th>DPI</th>
 							<th>NOMBRE</th>
 							<th>TELEFONO</th>
 							<th>DIRECCIÓN</th>
